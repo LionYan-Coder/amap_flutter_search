d52d127bd77fe9c5a85c71a9cfe32a8a8077532f
yanyahonglong <yanyahonglong@gmail.com>
2024-11-27 22:22:02 +0800

    .

--- android/src/main/java/com/example/amap_flutter_search/AmapFlutterSearchPlugin.java
+++ android/src/main/java/com/example/amap_flutter_search/AmapFlutterSearchPlugin.java
@@ -9,15 +9,20 @@ import com.amap.api.services.core.PoiItem;
 import com.amap.api.services.core.ServiceSettings;
 import com.amap.api.services.poisearch.PoiResult;
 import com.amap.api.services.poisearch.PoiSearch;
-import io.flutter.plugin.common.MethodCall;
-import io.flutter.plugin.common.MethodChannel;
-import io.flutter.plugin.common.MethodChannel.Result;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
 
+import io.flutter.Log;
+import io.flutter.embedding.engine.plugins.FlutterPlugin;
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
 
 
 
